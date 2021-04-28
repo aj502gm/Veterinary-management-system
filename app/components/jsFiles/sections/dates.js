4bc40b5656f8e0657773a9f27deec78fe4b42311
aj502gm <androsogt@gmail.com>
2021-04-28 11:19:22 -0600

    Visual Changes

--- app/components/jsFiles/sections/dates.js
+++ app/components/jsFiles/sections/dates.js
@@ -6,13 +6,15 @@ export default class{
     async getHTML(){
         return `
             <h1 class = "main-banner">Dates</h1>
-            <form class = "new-date ">
+            <form class = "new-date" method= "post">
                 <h3>Owner</h3>
                 <select class = "form-element global" >
                 </select>
                 <h3>Pet</h3>
                 <select class = "form-element global">
                 </select>
+                <h3>Schedule the date</h3>
+                <input type="date" id="birthday" name="birthday" class = "form-element global">
                 <h3>Description</h3>
                 <textarea class = "form-element description-text" placeholder= "What is wrong with your pet?" value = "" ></textarea><br>
                 <input type="submit" class = "form-element btn" value="Add">
